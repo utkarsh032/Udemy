f99af85124f7fc2ae5e880ae2a9b67ef4051f516
utkarsh032 <utkarshraj525@gmail.com>
2024-12-27 00:02:15 +0530

    Created Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar'
 import OfferCard from './components/OfferCard'
 
 function App() {
   return (
     <>
       <OfferCard />
+      <Navbar />
       <h1>Udemy</h1>
     </>
   )
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import Logo from '../assets/logo-udemy.svg'
+import { IoMdSearch } from "react-icons/io";
+import { Link } from 'react-router-dom'
+import { FaRegHeart } from "react-icons/fa6";
+import { FaOpencart } from "react-icons/fa";
+import { IoNotificationsOutline } from "react-icons/io5";
+
+
+
+
+export default function Navbar() {
+  return (
+    <div className='p-4 border-b'>
+      <div className='flex items-center justify-between'>
+        <img src={Logo} width='7%' alt="Logo" className='h-auto' />
+
+        <Link className="text-md   transition-colors duration-200">Explore</Link>
+
+        <div className='flex items-center border border-gray-300 rounded-full text-md gap-2 w-1/2   bg-gray-50 shadow-sm'>
+          <IoMdSearch className='text-2xl mx-2 text-gray-600' />
+          <input
+            className='outline-none border-none p-2 w-full bg-transparent placeholder-gray-500'
+            placeholder='Search for anything'
+          />
+        </div>
+
+        <Link className="text-md   transition-colors duration-200">Udemy Business</Link>
+
+        <Link className="text-md   transition-colors duration-200">My Learning</Link>
+
+        <Link className="text-xl  transition-colors duration-200">
+          <FaRegHeart />
+        </Link>
+
+        <Link className="text-xl  transition-colors duration-200 ">
+          <FaOpencart className='' />
+        </Link>
+
+        <Link className="text-xl  transition-colors duration-200">
+          <IoNotificationsOutline />
+        </Link>
+
+        <Link className="text-md   transition-colors duration-200">User</Link>
+      </div>
+    </div>
+
+  )
+}
