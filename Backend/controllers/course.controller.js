6849da9b3a70cea807f8ca29f0bb3d5c4b7b56d0
keshavsingh4093 <keshavsingh0818558@gmail.com>
2024-12-28 17:40:08 +0530

    get-course added in course router

--- Backend/controllers/course.controller.js
+++ Backend/controllers/course.controller.js
@@ -1,1 +1,13 @@
-import express from "express";
+import { Course } from "../models/course.model.js";
+
+const getAllCourses = async (req, res) => {
+    try {
+        const courses = await Course.find();
+
+        res.status(200).json(courses);
+    } catch (error) {
+        res.status(400).json({message:"Something went wrong"})
+    }
+}
+
+export { getAllCourses };
