3e1e0e892ec9d76d9550886dfb795c22478f18ff
utkarsh raj <utkarshraj525@gmail.com>
2025-01-19 22:17:20 +0530

    Update Structure with concurrently

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,11 @@
-import { courseRouter } from "./routes/course.router.js";
-import { userRouter } from "./routes/user.router.js";
-import { dbConnection } from "./dbConnection.js";
+import { courseRouter } from "./API/routes/course.router.js";
+import { userRouter } from "./API/routes/user.router.js";
 import fileUpload from "express-fileupload";
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { dbConnection } from "./API/config/dbConnection.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -27,5 +27,5 @@ app.use("/course", courseRouter);
 
 app.listen(PORT, async () => {
     await dbConnection();
-    console.log(`Server started at http://localhost:${PORT} and https://udemy-j08o.onrender.com`);
+    console.log(`Server started at PORT: ${PORT} and LIVE: https://udemy-j08o.onrender.com`);
 });
