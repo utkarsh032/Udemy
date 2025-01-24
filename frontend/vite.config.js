b80bb426934f689db04c1bad3ba25580536ed815
utkarsh raj <utkarshraj525@gmail.com>
2025-01-24 10:18:00 +0530

    Update Build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv';
 
-dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
