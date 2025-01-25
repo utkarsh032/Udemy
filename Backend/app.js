9d3520829ff761732d4a423165787c53000a80f8
Keshav Singh <162109908+keshavsingh4093@users.noreply.github.com>
2025-01-25 09:20:16 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,7 @@ import "dotenv/config";
 import path from "path";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4999;
 
 const frontendUrl = "https://udemy-e-learning.netlify.app";
 
